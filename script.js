ffd7582509e9b065df6801a35edf2cf5a0370148
NarminaKarimova <97615411+NarminaKarimova@users.noreply.github.com>
2022-12-04 21:12:41 +0400

    Update script.js
    adding event listener to city button

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ const container = document.querySelector(".container"),
     refresh = container.querySelector("h1 i");
 
 //based on entered city
-//based on entered city
 citybtn.addEventListener("click", () => {
     if (inputCity.value != "") {
         apiRequest(inputCity.value);
